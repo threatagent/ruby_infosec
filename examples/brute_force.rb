dedeeae252b74e1185a15f521c5f7985236412d6
Erran Carey <me@errancarey.com>
2013-08-17 14:09:20 -0500

    Update brute_force.rb

--- examples/brute_force.rb
+++ examples/brute_force.rb
@@ -9,10 +9,16 @@ class BruteForce
       exit 1
     end
 
+    # Only poll the wildcard domain once
+    wildcard_domain = check_for_wildcard(domain)
+    
     word_list.each do |word|
       begin
         address = Resolv.getaddress("#{word}.#{domain}")
-        puts "#{word}.#{domain} - #{address}"
+
+        unless address.eql? wildcard_domain
+          puts "#{word}.#{domain} - #{address}"
+        end
       rescue Resolv::ResolvError => re
         warn re.message
       rescue StandardError => se
@@ -21,8 +27,16 @@ class BruteForce
     end
   end
 
+  def check_for_wildcard(domain)
+    Resolv.getaddress("asdjlamsdklmasdnoemfjvcn.#{domain}")
+  rescue Resolv::ResolvError => re
+    # This error means that the domain isn't using wildcard subdomains
+    # Silently ignore this error and allow nil to be returned
+  end
+  
   def word_list
-    ['www', 'blog', 'mail', 'owa']
+    # Split defaults to splitting on white space
+    File.read('../resources/subdomains.txt').split
   end
 end
 
